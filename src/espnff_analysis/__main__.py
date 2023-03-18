8048eb6a319e9a441d7fe8f7bb27243a15afec9f
Jonathan Cheng <jonathancheng@Jonathans-MacBook-Pro.local>
2023-03-18 00:05:14 -0700

    main argparse init

--- src/espnff_analysis/__main__.py
+++ src/espnff_analysis/__main__.py
@@ -1,5 +1,56 @@
 import nfl_schedule as ns
+import ff_league_data as ff
+import calc_best_waiver as calc
+import argparse
+from espnff_analysis import __version__
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="ESPN Fantasy Football Analysis Pipeline",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--year",  # year of interest for FF analysis
+        "-y",
+        type=str,
+        help="Year of interest",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--league-id",
+        "l",
+        type=str,
+        help="League ID",
+        required=True,
+    )
+
+    parser.add_argument(
+        "swid",
+        "s",
+        type=str,
+        help="SWID",
+        required=True,
+    )
+
+    parser.add_argument(
+        "s2",
+        "t",
+        type=str,
+        help="s2",
+        required=True,
+    )
+
+    parser.add_argument("--version", "-v", action="version", version=__version__)
+
+    return parser
+
+def main():
+    pass
 
 if __name__ == "__main__":
-    df_final = ns.get_nfl_schedule(2022)
-    print(df_final)
+    main()
+
+
